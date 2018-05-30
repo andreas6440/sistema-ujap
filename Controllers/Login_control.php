2898ea94e6d60c06a6872cffcdced3fe4ce52de9
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-05-30 19:37:10 -0400

    nada nuevo

--- Controllers/Login_control.php
+++ Controllers/Login_control.php
@@ -11,49 +11,54 @@ class Login_control
 
 /*Alcides estuvo aqui*/
 
-    public function loguear(){
+    public function loguear()
+    {
 
         require_once "Models/Login_model.php";
 
         $login_model = new Login_model;
 
         if (isset($_REQUEST['submit'])) {
-            $usuario  = $_POST['username'];
+            $usuario = $_POST['username'];
+
             $password = md5($_POST['password']);
-            $data     = array("usuario" => $usuario, "password" => $password);
-            $log      = $login_model->logueo($data);
-            if(isset($log)){
+
+            $data = array("usuario" => $usuario, "password" => $password);
+            $log  = $login_model->logueo($data);
+
+            if (isset($log)) {
                 while ($row = pg_fetch_assoc($log)) {
                     $_SESSION['user'] = $row['usuario'];
-                    if(isset($_SESSION['user'])){
-                        $_SESSION['rut']='Perfil';
+                    if (isset($_SESSION['user'])) {
+                        $_SESSION['rut'] = 'Perfil';
                         echo '<meta http-equiv="Refresh" content="0;URL=index.php">';
                     }
                 }
             }
         }
-            
+
     }
-    
-    public function registrar(){
-        
+
+    public function registrar()
+    {
+
         require_once "Models/Login_model.php";
-        
+
         $login_model = new Login_model;
-        
+
         if (isset($_REQUEST['registrar'])) {
             $usuario  = $_POST['username-r'];
             $password = md5($_POST['password-r']);
-            $cedula  = $_POST['cedula'];
-            $nombre = $_POST['nombre'];
-            $apellido  = $_POST['apellido'];
+            $cedula   = $_POST['cedula'];
+            $nombre   = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
             $telefono = $_POST['telefono'];
-            $email = $_POST['email'];
+            $email    = $_POST['email'];
             $data     = array("usuario" => $usuario, "password" => $password, "cedula" => $cedula, "nombre" => $nombre, "apellido" => $apellido, "telefono" => $telefono, "email" => $email);
             $reg      = $login_model->registrar($data);
-            
+
         }
-        
+
     }
 
 }
