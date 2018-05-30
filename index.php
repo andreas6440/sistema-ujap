2b269a628856938d32432c45d7dd11074eff2ab4
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-05-30 10:24:34 -0400

    nada nuevo

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
+
 session_start();
 require_once 'Views/modulo/head.php';
 
-
 require_once 'Controllers/Rutas_control.php';
-if(isset($_GET['p'])){
-    $_SESSION['rut']=$_GET['p'];
+if (isset($_GET['p'])) {
+    $_SESSION['rut'] = $_GET['p'];
 }
-if(!isset($_SESSION['rut'])){
-    $_SESSION['rut']='Login';
+if (!isset($_SESSION['rut'])) {
+    $_SESSION['rut'] = 'Login';
 }
 rutear($_SESSION['rut']);
 
 require_once 'Views/modulo/fbody.php';
-
-?>
