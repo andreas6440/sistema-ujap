04fb05597fd2143e4e3ad786b445317750af0a9b
leitorino <leitorino_ajl2@hotmail.com>
2018-06-23 21:04:47 -0400

    algunas cosas al recibo

--- Controllers/PDF_control.php
+++ Controllers/PDF_control.php
@@ -1,14 +1,92 @@
 <?php
 
-session_start();
+require_once('Models/PDF_model.php');
+require_once('fpdf.php');
 
-require_once('../Models/PDF_model.php');
-require_once('../fpdf.php');
+class pdf{
+    
+    public function generarpdf($type){
+        if($type==1){
+            $drow = new dpdf;
+            $row = $drow->buscarRecibo();
 
+            $pdf = new FPDF;
 
-$pdf = new pdf;
+            $pdf->AddPage('P', 'Letter', '0');
+            $pdf->SetFont('Arial', '', 12);
+            $pdf->SetTitle('Recibo', TRUE);
+            $pdf->Image('Views/Assets/img/recibo.jpg', 0, 0, -300);
 
-$pdf->generar();
+            //Periodo Desde
+            $pdf->SetXY(103,57.5);
+            $pdf->Write(5,"Hola mundo");
+
+            //sm
+            $pdf->SetXY(142,57.5);
+            $pdf->Write(5,"Hola mundo");
+
+             //sd
+            $pdf->SetXY(180,57.5);
+            $pdf->Write(5,"Hola mundo");
+
+             //periodo hasta
+            $pdf->SetXY(103,64);
+            $pdf->Write(5,"Hola mundo");
+
+            //1era q
+            $pdf->SetXY(148,64);
+            $pdf->Write(5,"Hola mundo");
+
+            //num cta
+            $pdf->SetXY(87,70);
+            $pdf->Write(5,"Hola mundo");
+
+            //2da q
+            $pdf->SetXY(158,70.5);
+            $pdf->Write(5,"Hola mundo");
+
+            //Nombre
+            $pdf->SetXY(33,81);
+            $pdf->Write(5,$row['nombre']." ".$row['apellido']);
+
+            //Cedula
+            $pdf->SetXY(123,81);
+            $pdf->Write(5,$row['ci']);
+
+            //Ingreso
+            $pdf->SetXY(175,80.5);
+            $pdf->Write(5,$row['fecha_ingreso']);
+
+            //Departamento
+            $pdf->SetXY(45,87);
+            $pdf->Write(5,$row['departamento']);
+
+            //Dedicacion
+            $pdf->SetXY(159,87);
+            $pdf->Write(5,$row['dedicacion']);
+
+            //Cargo
+            $pdf->SetXY(28,95);
+            $pdf->Write(5,$row['cargo']);
+
+            //Categoria
+            $pdf->SetXY(148,94.8);
+            $pdf->Write(5,$row['categoria_nomina']);
+
+            //total
+            $pdf->SetXY(130,185);
+            $pdf->Write(5,"Hola mundo");
+
+            //total neto
+            $pdf->SetXY(141,198);
+            $pdf->Write(5,"Hola mundo");
+
+            $pdf->output('I','Recibo');
+            return $pdf;
+        }  
+    }
+    
+}
 
 
 
--- Views/Recibo.php
+++ Views/Recibo.php
@@ -1,16 +1,14 @@
+
 <section class="tables">   
             <div class="container-fluid">
               <div class="row">
                 <div class="col-lg-12">
                   <div class="card">
                     <div class="card-close">
-                      <div class="dropdown">
-                        <!--======================
-                            posiblemente lo quite
-                            =====================-->
+                      <!--<div class="dropdown">
                         <button type="button" id="closeCard1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-ellipsis-v"></i></button>
                         <div aria-labelledby="closeCard1" class="dropdown-menu dropdown-menu-right has-shadow"><a href="#" class="dropdown-item remove"> <i class="fa fa-times"></i>Close</a><a href="#" class="dropdown-item edit"> <i class="fa fa-gear"></i>Edit</a></div>
-                      </div>
+                      </div>-->
                     </div>
                     <div class="card-header d-flex align-items-center">
                       <h3 class="h4">Tabla de Recibos de pago</h3>
@@ -23,32 +21,31 @@
                               <th>#</th>
                               <th>Nº de recibo
                               </th>
-                              <th>Fecha</th>
-                              <th>info</th>
-                              <th>Monto</th>
-                              <th>opción</th>
+                              <th>Asunto</th>
+                              <th>Estado</th>
+                              <th>Recibido</th>
+                              <th>Revisado</th>
+                              <th>Seleccionar</th>
                             </tr>
                           </thead>
                           <tbody>
                             <tr>
                               <th scope="row">1</th>
-                              <td>Mark</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                              <td>@mdo</td>
+                              <td>146546</td>
+                              <td>Mes de enero</td>
+                              <td>Sin Revisar</td>
+                              <td>23/06/2018</td>
+                              <td></td>
+                              <td><input class="button" type="submit" value="Seleccionar"></td>
                             </tr>
                             <tr>
                               <th scope="row">2</th>
-                              <td>Jacob</td>
-                              <td>Thornton</td>
-                              <td>@fat</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">3</th>
-                              <td>Larry</td>
-                              <td>the Bird</td>
-                              <td>@twitter</td>
+                              <td>146587</td>
+                              <td>Mes de enero</td>
+                              <td>Revisado</td>
+                              <td>23/06/2018</td>
+                              <td>23/06/2018</td>
+                              <td><input class="button" type="submit" value="Seleccionar"></td>
                             </tr>
                           </tbody>
                         </table>
@@ -56,7 +53,7 @@
                     </div>
                   </div>
                 </div>
-                
+               
                 
               </div>
             </div>
