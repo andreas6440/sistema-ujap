11691aab8f40fbe1600dcc7697631796a85171e7
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-07-04 19:18:23 -0400

    nuevas modificaciones de perfil

--- Views/Perfil.php
+++ Views/Perfil.php
@@ -1,6 +1,6 @@
 <div class="container-fluid my-5 punto  ">
               <div class="row wrapper">
-                <div class="col-12">
+                <div class="col-lg-8">
                   <div class="card ">
                     
                     <div class="card-header d-flex align-items-center">
@@ -45,57 +45,24 @@
                                                     </div>
                                                 </div>
                                             </div>
-
-                                         <div class="row">
-                                            <div class="col-md-5">
+                                        <div class="row">
+                                            <div class="col-md-12">
                                                 <div class="group">
                                                 <input class="input2" required="required" type="text"/>
 
                                                         <span class="bar">
                                                         </span>
                                                         <label class="label2">
-                                                            Departamento
+                                                            Dirección
                                                         </label>
                                             </div>
                                         </div>
-                                            <div class="col-md-3">
-                                                <div class="group">
-                                                <input class="input2" required="required" type="text"/>
-
-                                                        <span class="bar">
-                                                        </span>
-                                                        <label class="label2">
-                                                            Cargo
-                                                        </label>
-                                            </div>
-                                        </div>
-                                            <div class="col-md-4">
-                                                <div class="group">
-                                                <input class="input2" required="required" type="text"/>
-
-                                                        <span class="bar">
-                                                        </span>
-                                                        <label class="label2">
-                                                            Categoría
-                                                        </label>
-                                            </div>
-                                        </div>
-                                        </div>
+                                    </div>
 
                                     <div class="row">
 
 
-                                        <div class="col-md-3">
-                                                <div class="group">
-                                                <input class="input2" required="required" type="text"/>
-
-                                                        <span class="bar">
-                                                        </span>
-                                                        <label class="label2">
-                                                            Fecha de Ingreso
-                                                        </label>
-                                            </div>
-                                        </div>
+                                        
                                         <div class="col-md-5">
                                                 <div class="group">
                                                 <input class="input2" required="required" type="text"/>
@@ -121,19 +88,7 @@
 
 
                                     </div>
-                                    <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="group">
-                                                <input class="input2" required="required" type="text"/>
-
-                                                        <span class="bar">
-                                                        </span>
-                                                        <label class="label2">
-                                                            Dirección
-                                                        </label>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    
                                     <div class="row">
                                             <div class="col-md-4 ">
                                                 <button type="button" class="btn color-azul btn-submit btn-lg" disabled>Guardar Cambios</button>
@@ -143,11 +98,7 @@
   </div>
 </div>
 </div>
-</div>
-</div>
-<div class="container-fluid my-0 punto  ">
-              <div class="row wrapper">
-                <div class="col-12">
+<div class="col-lg-4 col-md-12">
                   <div class="card ">
                     
                     <div class="card-header d-flex align-items-center">
@@ -157,7 +108,7 @@
                     <div class="card-body ">
                       <form>
                                             <div class="row">
-                                                <div class="col-md-2">
+                                                <div class="col-12">
                                                     <div class="group">
                                                         <input class="input2" required="required" type="text"/>
                                                         <span class="bar">
@@ -168,7 +119,9 @@
 
                                                     </div>
                                                 </div>
-                                                <div class="col-md-4">
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-12">
                                                     <div class="group">
                                                         <input class="input2" required="required" type="text"/>
 
@@ -180,7 +133,9 @@
 
                                                     </div>
                                                 </div>
-                                                <div class="col-md-4">
+                                                </div>
+                                                <div class="row">
+                                                <div class="col-12">
                                                     <div class="group">
                                                         <input class="input2" required="required" type="text"/>
 
@@ -191,14 +146,18 @@
                                                         </label>
                                                     </div>
                                                 </div>
-                                                <div class="col-md-2 my-5">
+                                            </div>
+                                                <div class="row">
+                                                <div class="col-md-2 mx-2">
                                                   <button type="button" class="btn color-azul btn-submit btn-lg" disabled>Guardar Cambios</button>
                                                 </div>
                                                 
                                             </div>
 
-                      </div>
+                      
+                  </form>
                     </div>
                   </div>
-                </div>
-              </div>
+</div>
+</div>
+</div>
