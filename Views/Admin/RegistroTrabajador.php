98c92e607ec4d72914b59c21c7e98b96e1d8d5e6
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-07-05 22:27:00 -0400

    perfil RH

---
+++ Views/Admin/RegistroTrabajador.php
@@ -0,0 +1,148 @@
+<div class="container-fluid my-5 punto  ">
+              <div class="row wrapper">
+                <div class="col-lg-12">
+                  <div class="card ">
+                    
+                    <div class="card-header d-flex align-items-center">
+                      <h3 class="h4 mx-2">Nuevo usuario de recursos humanos </h3>
+
+                    </div>
+                    <div class="card-body ">
+                      <form>
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <div class="group">
+                                                        <input class="input2" required="required" type="text"/>
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            cédula
+                                                        </label>
+
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <div class="group">
+                                                        <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Nombre
+                                                        </label>
+
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <div class="group">
+                                                        <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Apellido
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-3">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            correo electronico
+                                                        </label>
+                                            </div>
+                                        </div>
+                                            </div>
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            teléfono
+                                                        </label>
+                                            </div>
+                                        </div>
+                                            <div class="col-md-5">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Dirección
+                                                        </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Nombre Usuario
+                                                        </label>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    
+                                    <div class="row">
+
+
+                                        
+                                        <div class="col-md-4">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Contraseña
+                                                        </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Repetir Contraña
+                                                        </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                                <div class="group">
+                                                <input class="input2" required="required" type="text"/>
+
+                                                        <span class="bar">
+                                                        </span>
+                                                        <label class="label2">
+                                                            Nombre Usuario
+                                                        </label>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                    <div class="row">
+                                            <div class="col-md-4 ">
+                                                <button type="button" class="btn color-azul btn-submit btn-lg" disabled>Registrar</button>
+                                            </div>
+                                        </div>
+                                    </form>
+  </div>
+</div>
+</div>
+
+</div>
+</div>
