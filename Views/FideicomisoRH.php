fe714644f83761651f5ff1ced3c2face0b585f14
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-07-08 16:15:37 -0400

    otro cambio

--- Views/FideicomisoRH.php
+++ Views/FideicomisoRH.php
@@ -1,11 +1,29 @@
 <div class="container-fluid my-5 punto  ">
+   <h5 class=" pestaña"><i class="fa fa-filter" ></i> Recursos Humanos/ Fideicomiso</h5>
               <div class="row wrapper">
                 <div class="col-12">
                   <div class="card ">
                     
                     <div class="card-header d-flex align-items-center">
-                      <h3 class="h4 mx-2">Fideicomiso</h3>
-
+                      <div class="container-fluid">
+                        <div class="row">
+                          <div class="col-lg-6 col-sm-12  ">
+                      <h3 class="h4 mx-2 py-2 ">Fideicomiso</h3>
+                      </div>
+                      <div class="col-lg-6 col-sm-12 ">
+                      <div class="select ml-auto mr-sm-auto">
+                        <select id="sele" name="slct" id="slct">
+                          <option>Seleccione el año</option>
+                          <option value="1">2018</option>
+                          <option value="2">2017</option>
+                          <option value="3">2016</option>
+                          <option value="3">2015</option>
+                          <option value="3">2014</option>
+                        </select>
+                      </div>
+                    </div >
+                    </div>
+                    </div>
                     </div>
                     <div class="card-body ">
                       <table class="responsive-table table-hover">
@@ -43,7 +61,24 @@
     </tbody>
   </table>
                     </div>
-
+                    <div class="card-footer">
+                      <div class="pagination-wrapper">
+  <div class="pagination">
+    <a class="prev page-numbers" href="javascript:;">prev</a>
+    <span aria-current="page" class="page-numbers current">1</span>
+    <a class="page-numbers" href="javascript:;">2</a>
+    <a class="page-numbers" href="javascript:;">3</a>
+    <a class="page-numbers" href="javascript:;">4</a>
+    <a class="page-numbers" href="javascript:;">5</a>
+    <a class="page-numbers" href="javascript:;">6</a>
+    <a class="page-numbers" href="javascript:;">7</a>
+    <a class="page-numbers" href="javascript:;">8</a>
+    <a class="page-numbers" href="javascript:;">9</a>
+    <a class="page-numbers" href="javascript:;">10</a>
+    <a class="next page-numbers" href="javascript:;">next</a>
+  </div>
+</div>
+                    </div>
                 </div>
                 
               </div>
