6044aec7beebd30665da19120447f40d2138f059
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-07-08 16:22:31 -0400

    bono alimenticio y recibo de pago

---
+++ Views/RepoAlimenticioRH.php
@@ -0,0 +1,87 @@
+<div class="container-fluid my-5 punto  ">
+  <h5 class=" pestaña"><i class="fa fa-filter" ></i> Recursos Humanos/ Bono Alimenticio</h5>
+              <div class="row wrapper">
+                <div class="col-12">
+                  <div class="card ">
+                    
+                    <div class="card-header d-flex align-items-center">
+                      <div class="container-fluid">
+                        <div class="row">
+                          <div class="col-lg-6 col-sm-12  ">
+                      <h3 class="h4 mx-2 py-2 ">Reportes de Bono Alimenticio</h3>
+                      </div>
+                      <div class="col-lg-6 col-sm-12 ">
+                      <div class="select ml-auto mr-sm-auto">
+                        <select id="sele" name="slct" id="slct">
+                          <option>Seleccione el año</option>
+                          <option value="1">2018</option>
+                          <option value="2">2017</option>
+                          <option value="3">2016</option>
+                          <option value="3">2015</option>
+                          <option value="3">2014</option>
+                        </select>
+                      </div>
+                    </div >
+                    </div>
+                    </div>
+                    </div>
+                    <div class="card-body ">
+                      <table class="responsive-table table-hover">
+    
+    <thead>
+      <tr class="h5 text-center">
+        
+                              <th scope="col">#</th>
+                              <th scope="col">Nº de Reporte de Bono Alimenticio
+                              </th>
+                              <th scope="col">Fecha de emisión</th>                              
+                              <th scope="col">Seleccionar</th>
+                            
+      </tr>
+    </thead>
+   
+    <tbody>
+      
+      <tr>
+        <th scope="row">1</th>
+        <td data-title="Fecha">15-1-2018</td>
+        <td data-title="Nº de ARC">Universal</td>
+        
+        <td data-title="Info" ><button class="button type1">
+                                 <i class="fa fa-file-pdf-o" aria-hidden="true"></i> Ver
+                                </button></td>
+     
+    
+    </tr>
+
+     
+     
+  
+      
+    </tbody>
+  </table>
+                    </div>
+<div class="card-footer">
+                      <div class="pagination-wrapper">
+  <div class="pagination">
+    <a class="prev page-numbers" href="javascript:;">prev</a>
+    <span aria-current="page" class="page-numbers current">1</span>
+    <a class="page-numbers" href="javascript:;">2</a>
+    <a class="page-numbers" href="javascript:;">3</a>
+    <a class="page-numbers" href="javascript:;">4</a>
+    <a class="page-numbers" href="javascript:;">5</a>
+    <a class="page-numbers" href="javascript:;">6</a>
+    <a class="page-numbers" href="javascript:;">7</a>
+    <a class="page-numbers" href="javascript:;">8</a>
+    <a class="page-numbers" href="javascript:;">9</a>
+    <a class="page-numbers" href="javascript:;">10</a>
+    <a class="next page-numbers" href="javascript:;">next</a>
+  </div>
+</div>
+                    </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+         
