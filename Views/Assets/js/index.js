29d25d05deb9e1cb3b9072c74bc7fe63daa77741
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-07-15 18:54:18 -0400

    modal

--- Views/Assets/js/index.js
+++ Views/Assets/js/index.js
@@ -62,6 +62,7 @@ $('#NumDocumento').remove();
 });
 }*/
 if ($('#banner').length) {
+  $('#exampleModal').appendTo("body");
   $('#OlClave').click(function() {
    var closable = alertify.alert().setting('closable');
 
@@ -71,5 +72,9 @@ alertify.alert()
     'message': '<div class="gru-minuscula">En caso de haber olvidado su contraseña, favor dirigirse a la oficina de redes para su posterior reestablecimiento.<div>' ,
     'onok': function(){ alertify.success('excelente');}
   }).show().setHeader('<div class="brand-text brand-big "><span>Universidad José Antonio Páez </span></div>');; });
+ 
+
   }
+
+
 });
