bc5fef7a24ef52deb08764bc6a495307e8b569b1
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2018-07-15 21:21:01 -0400

    se llama acceso

--- Views/Login.php
+++ Views/Login.php
@@ -51,46 +51,10 @@ if (isset($_POST['registrar'])) {
                                         <div class="form-group">
                                             <input class="boton-azul" type="submit" name="submit" id="submit" value="Entrar">
                                         </div>
-                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+                                    
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Univeridad José Antonio Páez</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          <form class="form">
-  
-  
-  <center><h5>Elegir tipo de usuario</h5></center>
-  <div class="inputGroup">
-    <input id="radio1" name="radio" type="radio"/>
-    <label for="radio1">General</label>
-  </div>
-  <div class="inputGroup">
-    <input id="radio2" name="radio" type="radio"/>
-    <label for="radio2">Recursos Humanos</label>
-  </div>
-  <div class="inputGroup">
-    <input id="radio3" name="radio" type="radio"/>
-    <label for="radio3">Admin</label>
-  </div>
-</form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-        <button type="button" class="btn btn-primary">Aceptar</button>
-      </div>
-    </div>
-  </div>
-</div>
+
                                     </form>
 
                                 </div>
---
+++ Views/acceso.php
@@ -0,0 +1,47 @@
+        <section id="banner" >
+
+            <div class="bg-color ">
+                <div class="container-fluid">
+                    <div class="materialContainer">
+                        <div class="form">
+                            <div class="form-toggle">
+                            </div>
+                            <div class="form-panel one">
+                                <div class="form-header">
+                                    <h1>
+                                        Elegir tipo de usuario
+                                    </h1>
+                                </div>
+                                <div class="form-content mr-auto">
+                                    <form method="post" class="form2">                                  
+  
+  
+  
+  <div class="inputGroup">
+    <input id="radio1" name="radio" type="radio"/>
+    <label for="radio1">General</label>
+  </div>
+  <div class="inputGroup">
+    <input id="radio2" name="radio" type="radio"/>
+    <label for="radio2">Recursos Humanos</label>
+  </div>
+  <div class="inputGroup">
+    <input id="radio3" name="radio" type="radio"/>
+    <label for="radio3">Admin</label>
+  </div>
+
+                                        <div class="form-group">
+                                            <input class="boton-azul" type="submit" name="submit" id="submit" value="Entrar">
+                                        </div>
+
+                                    </form>
+
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
