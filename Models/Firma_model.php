da5335a088ab08a5fc510a06924253be8d417c5f
leitorino <leitorino_ajl2@hotmail.com>
2018-08-06 21:18:08 -0400

    firma

--- Models/Firma_model.php
+++ Models/Firma_model.php
@@ -6,12 +6,23 @@ function actualizarFirmam($datos){
         
     if (isset($datos["cedula"])){
                 
-        $query = "UPDATE public.".'"Firma"'." SET cedula = $1, nombre = $2, apellido = $3 WHERE id=1;";
+        $query = "SELECT cedula FROM public.".'"UsuariosWeb"'." where cedula = $1 and nombre = $2 and apellido = $3;";
         $prepared = pg_prepare($dbcon, "", $query);
         $prepared = pg_execute($dbcon, "", array(
             $datos["cedula"],
             $datos["nombre"],
-            $datos["apellido"]));           
+            $datos["apellido"]
+        ));
+        
+        if(pg_fetch_assoc($prepared)['cedula']){
+            $query = "UPDATE public.".'"Firma"'." SET cedula = $1, nombre = $2, apellido = $3 WHERE id=1;";
+            $prepared = pg_prepare($dbcon, "", $query);
+            $prepared = pg_execute($dbcon, "", array(
+            $datos["cedula"],
+            $datos["nombre"],
+            $datos["apellido"]));
+        }
+                   
     }  
 }
 
